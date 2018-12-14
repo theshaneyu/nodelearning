adc706d86bb2b495ca84df42347983718703e4bc
Shane Yu <shaneyu07@gmail.com>
2018-12-14 17:55:23 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+// function sayHi(){
+//     console.log('say hi')
+// }
+
+// sayHi()
+
+// var sayBye = function(){
+//     console.log('Say Bye')
+// }
+
+// sayBye()
+
+function callFunction(fun){
+    fun()
+}
